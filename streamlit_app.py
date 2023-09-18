d9f145db3b1f4e8023855d2d248755f0ea4fabf1
floe-prod <144444549+floe-prod@users.noreply.github.com>
2023-09-18 16:54:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ streamlit.write('The user entered ', fruit_choice)
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
-    streamlit.datafram(my_data_rows)
+    streamlit.dataframe(my_data_rows)
 
 streamlit.stop()
 # Add a textbox, so user can manually add a fruit to list
