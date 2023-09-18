54194a4229a9a49ce5bbfdabfadc441ec0a26237
floe-prod <144444549+floe-prod@users.noreply.github.com>
2023-09-18 14:20:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,16 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-# normalize json data 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# push to frontend
-streamlit.dataframe(fruityvice_normalized)
-# streamlit.text(fruityvice_response)
+
 streamlit.header("Fruityvice Fruit Advice!")
 
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+# normalize json data 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# push to frontend
+streamlit.dataframe(fruityvice_normalized)
+# streamlit.text(fruityvice_response)
+
 streamlit.write('The user entered ', fruit_choice)
