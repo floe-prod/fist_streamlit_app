89e4ea19c025ac1fe56ca7c176f299ac148397a3
floe-prod <144444549+floe-prod@users.noreply.github.com>
2023-09-18 16:42:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-def get_fruityvice_data(this_fruit_chioce:str): -> normalized_clean_data
+def get_fruityvice_data(this_fruit_chioce:str):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_chioce)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
