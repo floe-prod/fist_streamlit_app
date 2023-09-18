90a30136c58a8e2aca9b60972c4d1f93eb58fc73
floe-prod <144444549+floe-prod@users.noreply.github.com>
2023-09-18 14:10:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,12 +20,12 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.text(fruityvice_response)
 
-
-
-
+# normalize json data 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# push to frontend
+streamlit.dataframe(fruityvice_normalized)
 
 streamlit.dataframe(fruits_to_show)
-
-
+streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
