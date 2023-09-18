562a241ed3f1470c93bf8e1647e612f0a8f5439e
floe-prod <144444549+floe-prod@users.noreply.github.com>
2023-09-18 14:18:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
 streamlit.title('Tutorial for my parents healty restaurant!')
@@ -28,5 +27,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 # streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
+
+
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.write('The user entered ', fruit_choice)
