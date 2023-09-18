54a2381ff4a7e1d9132f328647ad607ace895e7e
floe-prod <144444549+floe-prod@users.noreply.github.com>
2023-09-18 16:23:56 +0200

    Update streamlit_app.py
    added stop for steamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.write('The user entered ', fruit_choice)
 
-
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
